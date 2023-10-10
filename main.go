6fb56b12130630af62d51ffb8f224a9acdb6e6e2
Timothy Cates <Tim@arch.localdomain>
2023-10-10 13:06:43 -0700

    Initial Project Scaffhold
    Have the main program init a router
    have the router render a view (string)
    have the settings types defined

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/timothycates/Arch-Install-TUI/router"
+	tea "github.com/charmbracelet/bubbletea"
+)
+func main(){
+  var router router.RouterModel = router.New()
+  var program *tea.Program = tea.NewProgram(router)
+  if _, err := program.Run(); err != nil{
+		fmt.Fprintf(os.Stderr, "Error running arch-tui: %v\n", err)
+		os.Exit(1)
+  }
+
+}
---
+++ router/router.go
@@ -0,0 +1,25 @@
+package router
+import(
+
+  tea "github.com/charmbracelet/bubbletea"
+)
+
+type RouterModel struct{
+  activeController int
+}
+
+func New() RouterModel{
+  return RouterModel{}
+}
+
+func (r RouterModel) Update(msg tea.Msg) (tea.Model, tea.Cmd){
+  return r, nil
+}
+
+func (r RouterModel) View() string{
+  return "Router"
+}
+
+func (r RouterModel) Init() tea.Cmd{
+  return nil
+}
