029fb2af0335d21194fc9638782bbcb4859caea6
Timothy Cates <Tim@arch.localdomain>
2023-10-14 00:02:16 -0700

    Make Controller an interrface

--- internal/controllers/controller.go
+++ internal/controllers/controller.go
@@ -2,7 +2,8 @@ package controllers
 
 import tea "github.com/charmbracelet/bubbletea"
 
-type Controller struct{
-  Update func(tea.Msg) (tea.Model, tea.Cmd)
-  View func() string
+type Controller interface{
+  Update(tea.Msg) (tea.Model, tea.Cmd)
+  View() string
 }
+
